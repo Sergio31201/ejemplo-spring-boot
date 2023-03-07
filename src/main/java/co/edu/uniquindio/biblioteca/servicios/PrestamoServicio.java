58e697245b4be3830ade92104b03bf0768b9ae2b
sergio31201 <sergioe.latorreg@uqvirtual.edu.co>
2023-03-07 01:54:13 -0500

    CRUD de prestamos

---
+++ src/main/java/co/edu/uniquindio/biblioteca/servicios/PrestamoServicio.java
@@ -0,0 +1,71 @@
+package co.edu.uniquindio.biblioteca.servicios;
+
+import co.edu.uniquindio.biblioteca.dto.PrestamoDTO;
+import co.edu.uniquindio.biblioteca.entity.Cliente;
+import co.edu.uniquindio.biblioteca.entity.Libro;
+import co.edu.uniquindio.biblioteca.entity.Prestamo;
+import co.edu.uniquindio.biblioteca.repo.ClienteRepo;
+import co.edu.uniquindio.biblioteca.repo.PrestamoRepo;
+import co.edu.uniquindio.biblioteca.servicios.excepciones.ClienteNoEncontradoException;
+import co.edu.uniquindio.biblioteca.servicios.excepciones.PrestamoNoEncontradoException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class PrestamoServicio {
+
+    private final PrestamoRepo prestamoRepo;
+    private final ClienteRepo clienteRepo;
+
+    public Prestamo save(PrestamoDTO prestamoDTO){
+
+        long codigoCliente = prestamoDTO.clienteID();
+        Optional<Cliente> consulta = clienteRepo.findById(codigoCliente);
+
+        if(consulta.isEmpty()){
+            throw new ClienteNoEncontradoException("No existe");
+        }
+
+        Prestamo prestamo = new Prestamo();
+        prestamo.setCliente(consulta.get());
+        prestamo.setFechaPrestamo(LocalDateTime.now());
+
+        List<String> codigosLibros = prestamoDTO.isbnLibros();
+        List<Libro> libros = new ArrayList<>();
+
+        /*Optional<Libro> buscado libroRepo.findById(codigosLibros[0]);
+
+        if(buscado.isEmpty()){
+            throw new LibroNoExiste("El libro no existe");
+        }
+
+        libros.add( buscado );*/
+
+        //TODO Completar la parte de los libros
+        prestamo.setLibros(libros);
+        prestamo.setFechaDevolucion(prestamoDTO.fechaDevolucion());
+
+        return prestamoRepo.save(prestamo);
+    }
+
+
+    public List<Prestamo> findAll(){
+        return prestamoRepo.findAll();
+    }
+
+
+    public List<PrestamoDTO> findByCodigoCliente(long codigoCliente){
+        return (List<PrestamoDTO>) clienteRepo.findById(codigoCliente).orElseThrow(()-> new PrestamoNoEncontradoException("No tiene prestamos"));
+    }
+
+
+    public List<PrestamoDTO> findById(long codigoPrestamo){
+        return (List<PrestamoDTO>) prestamoRepo.findById(codigoPrestamo).orElseThrow(()-> new PrestamoNoEncontradoException("No existe"));
+    }
+
+}
---
+++ src/main/java/co/edu/uniquindio/biblioteca/servicios/excepciones/PrestamoNoEncontradoException.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.biblioteca.servicios.excepciones;
+
+public class PrestamoNoEncontradoException extends RuntimeException{
+    public PrestamoNoEncontradoException(String messaje){super(messaje);}
+}
